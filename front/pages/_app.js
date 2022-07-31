7d60363515e6ea944daf1999843df0c95d136cb0
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-07-31 23:49:53 +0100

    setup redux successfully

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { wrapper, store , persistor } from "../store/store";
+import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Provider store={store}>
+        <PersistGate persistor={persistor} >
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </>
+  );
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp);
