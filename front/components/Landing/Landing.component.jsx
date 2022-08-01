de999f90968d54fdd8292089065ce31952c0f0bc
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-08-01 22:53:57 +0100

    setup namecheap domain

--- front/components/Landing/Landing.component.jsx
+++ front/components/Landing/Landing.component.jsx
@@ -5,7 +5,7 @@ const Landing = () => {
     <Main>
       <Title>AYOUB<br/> KASSI</Title>
       <Description>Self-learning software and electrical engineering through research & development. Filming the entire process on YouTube.</Description>
-      <Info>57 projects - 12 videos</Info>
+      <Info>72 projects - 12 videos</Info>
     </Main>
 
   )
